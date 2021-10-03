46c42ef84d6bf6c37d3780497eff25774d9067e5
kamal1004 <89079657+kamal1004@users.noreply.github.com>
2021-10-03 16:40:59 +0530

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+
+  function calculateTotal() {
+    setTotal(number1 + number2);
+  }
+
+  return (
+    <div className="App">
+      <h1>Adding Two Numbers</h1>
+
+      <div className="number-inputs">
+        <input
+          type="number"
+          value={number1}
+          onChange={(e) => setNumber1(+e.target.value)}
+          placeholder="0"
+        />
+        <input
+          type="number"
+          value={number2}
+          onChange={(e) => setNumber2(+e.target.value)}
+          placeholder="0"
+        />
+      </div>
+
+      <button onClick={calculateTotal}>Add Them!</button>
+
+      <h2>{total}</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
